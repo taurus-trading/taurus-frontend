a1c3fd15f35a9c195a6c772b675ec2b70abfbf25
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-03-06 12:23:28 -0800

    create new file structure

---
+++ src/search-page/details/notesDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NoteDisplay extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/search-page/social/tweets.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TweetComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
