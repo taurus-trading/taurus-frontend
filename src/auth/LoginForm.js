99cab785f40f24358cfefbc3e53d869333fcbb80
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-03-08 15:44:40 -0800

    create login form, signup form, signin/signup page

---
+++ src/auth/LoginForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import loginUser from '../userUtils.js';
+
+
+export default class SignUpForm extends Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleEmail = (e) => {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePassword = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user = await loginUser();
+
+        this.props.handleUserChange(user);
+        this.props.history.push('/dashboard');
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email: 
+                        <input value={this.state.email} onChange={this.handleEmail}  />
+                    </label>
+                    <label>
+                        Password: 
+                        <input value={this.state.password} onChange={this.handlePassword}  />
+                    </label>
+
+                    <button>Sign Up</button>
+                </form>
+            </div>
+        )
+    }
+}
