cc6ad7c580ad122f16eef8f9c6d96ec2492e21ff
Dylan White <dylan.whitej@gmail.com>
2021-03-08 14:13:29 -0800

    test fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, skip } from '@testing-library/react';
 import App from './App';
 
 skip('renders learn react link', () => {
