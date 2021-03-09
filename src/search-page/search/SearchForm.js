b63407971eee2a0d5f79b23a1587a42920cb3795
Dylan White <dylan.whitej@gmail.com>
2021-03-09 11:41:18 -0800

    pre merge

--- src/search-page/search/SearchForm.js
+++ src/search-page/search/SearchForm.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react'
 
 export default class SearchForm extends Component {
     render() {
+        state = {
+            query: ''
+        }
+        handleInputChange = (e) => {
+            this.setState({
+                query: e.target.value,
+            })
+        }
+        submitHandler = (e) => {
+            e.preventDefault()
+            this.props.handleSearchSubmit(this.state.query);
+        }
         return (
             <div>
-                
+                <form>
+                    <label>
+                        Search for a ticker: 
+                        <input type="text" value={this.state.query} onChange={this.handleInputChange}/>
+                    </label>
+                    <button onSubmit={this.submitHandler}>Search</button>
+                </form>
             </div>
         )
     }
--- src/search-page/search/SearchSection.js
+++ src/search-page/search/SearchSection.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import { searchStocks } from '../../utils/api-utils'
+import SearchForm from './SearchForm'
+import SearchItem from './SearchItem.js'
 
 export default class SearchSection extends Component {
     render() {
+        state = {
+            searchResults: []
+        }
+        handleSearchSubmit = async (search) => {
+            const searchResults = await searchStocks(search);
+            this.setState({
+                searchResults
+            })
+        }
         return (
             <div>
-                
+                <SearchForm handleSubmit={this.handleSearchSubmit} />
+                <>
+                {
+                    this.state.searchResults.length >  0 ? this.state.map()<SearchItem />
+                }
+                </>
             </div>
         )
     }
