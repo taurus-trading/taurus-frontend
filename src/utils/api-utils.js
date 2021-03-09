f6b066bef323db4751043c64b66ca6ef2c9a975c
Dylan White <dylan.whitej@gmail.com>
2021-03-09 10:05:23 -0800

    fixes api utils

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -4,20 +4,34 @@ const BACK_END_URL = 'https://taurus-backend.herokuapp.com/';
 
 
 export async function getTwits(symbol) {
-    const response = request.get(`${BACK_END_URL}/twits?search=${symbol}`);
+    const response = await request.get(`${BACK_END_URL}/twits?search=${symbol}`);
+
+    return response.body;
+}
+export async function getTrending() {
+    const response = await request.get(`${BACK_END_URL}/trending`);
 
     return response.body;
 }
 export async function getCurrentStockPrice(symbol) {
-    const response = request.get(`${BACK_END_URL}/currentprice?search=${symbol}`);
+    const response = await request.get(`${BACK_END_URL}/currentprice?search=${symbol}`);
 
     return response.body;
 }
 export async function getStockPriceHistory(symbol, resolution, toDate, fromDate) {
-    const response = request.get(`${BACK_END_URL}/pricehistory?symbol=${symbol}&resolution=${resolution}&to=${toDate}&from=${fromDate}`);
+    const response = await request.get(`${BACK_END_URL}/pricehistory?symbol=${symbol}&resolution=${resolution}&to=${toDate}&from=${fromDate}`);
 
     return response.body;
 }
+export async function searchStocks(query) {
+    const response = await request.get(`${BACK_END_URL}/search?search=${query}`);
+
+    return response.body;
+}
+
+
+
+
 
 
 
