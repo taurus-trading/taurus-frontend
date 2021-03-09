4373a7fa00bc13133792505a6cb65461df39da60
Dylan White <dylan.whitej@gmail.com>
2021-03-08 16:51:27 -0800

    user-utils

---
+++ src/utils/api-utils.js
@@ -0,0 +1,20 @@
+const BACK_END_URL = 'https://taurus-backend.herokuapp.com/';
+
+export async function getTwits(symbol) {
+    const response = request.get(`${BACK_END_URL}/twits?search=${symbol}`);
+
+    return response.body;
+}
+export async function getCurrentStockPrice(symbol) {
+    const response = request.get(`${BACK_END_URL}/currentprice?search=${symbol}`);
+
+    return response.body;
+}
+export async function getStockPriceHistory(symbol, resolution, toDate, fromDate) {
+    const response = request.get(`${BACK_END_URL}/pricehistory?symbol=${symbol}&resolution=${resolution}&to=${toDate}&from=${fromDate}`);
+
+    return response.body;
+}
+
+
+
--- src/utils/user-utils.js
+++ src/utils/user-utils.js
@@ -2,6 +2,7 @@ import { request } from 'superagent'
 const USER = 'USER';
 const BACK_END_URL = 'https://taurus-backend.herokuapp.com/';
 
+//USER UTILS
 export async function signUpUser(email, password) {
     const response = await request.post(`${BACK_END_URL}/auth/signup`).send({email, password});
 
@@ -11,7 +12,7 @@ export async function signUpUser(email, password) {
 export async function fillUserNameAndDate(user_name, token) {
     const date = new Date();
     const inMillis = date.getTime();
-    const response = await request.post(`${BACK_END_URL}/updateuser`).send({user_name, inMillis, token});
+    const response = await request.post(`${BACK_END_URL}/updateuser`).send({user_name, inMillis}).set('Authorization', token);
 
     return response.body
 }
@@ -22,14 +23,43 @@ export async function loginUser(email, password) {
     return response.body;
 }
 
+
+// PORTFOLIO UTILS
 export async function getUserPortfolio(token) {
-    const response = request.get(`${BACK_END_URL}/api/portfolio`)
+    const response = request.get(`${BACK_END_URL}/api/portfolio`).set('Authorization', token)
 
     return response.body;
 }
 
+export async function addToPortfolio(token, symbol, title, date_purchased, quantity, current_price) {
+    const d = new Date();
+    const date_purchased = d.getTime();
+    const cost = quantity * current_price;
+    const response = request.post(`${BACK_END_URL}/api/portfolio`).set('Authorization', token).send({
+        symbol, title, quantity, date_purchased, cost, quantity, current_price
+    })
+
+    return response.body;
+}
+
+
+//WATCH LIST UTILS
 export async function getUserWatchList(token) {
-    
+    const response = request.get(`${BACK_END_URL}/api/watchlist`).set('Authorization', token);
+
+    return response.body;
+}
+export async function addToWatchList(token, symbol, title) {
+    const response = request.post(`${BACK_END_URL}/api/watchlist`).set('Authorization', token).send({
+        symbol, title
+    });
+
+    return response.body;
+}
+export async function deleteFromWatchList(token, id) {
+    const response = request.delete(`${BACK_END_URL}/api/watchlist/${id}`).set('Authorization', token);
+
+    return response.body;
 }
 
 //local storage functions
