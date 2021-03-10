9d46c9440f571fcb7e84083165d8eed47247c0c7
patrick hrabos <phrabos@gmail.com>
2021-03-10 15:52:58 -0500

    make separate page to create new user

--- src/auth/SignupPage.js
+++ src/auth/SignupPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 // import LoginForm from './LoginForm'
-import {LoginWithRouter} from './LoginForm.js';
+
 import { SignupWithRouter } from './SignupForm.js';
 import './LoginSignup.css';
 import BackgroundImage from './BackgroundImage.js';
 
-export default class LoginSignupPage extends Component {
+export default class SignupPage extends Component {
     state = {
         user: '',
     }
@@ -16,9 +16,7 @@ export default class LoginSignupPage extends Component {
             
             <div className='page-wrapper'>
                 <div className="user-form">
-                    <LoginWithRouter 
-                    handleUserChange={this.props.handleUserChange} />
-                    
+                    <SignupWithRouter handleUserChange={this.props.handleUserChange} />
                 </div>
                 <div className='video-div'>
                 <BackgroundImage />
