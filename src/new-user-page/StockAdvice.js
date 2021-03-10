de86de11895bcbad0be0ad2e8a6c36a59e15ba5c
patrick hrabos <phrabos@gmail.com>
2021-03-09 20:24:49 -0500

    css style of blog articles div

--- src/new-user-page/StockAdvice.js
+++ src/new-user-page/StockAdvice.js
@@ -14,7 +14,7 @@ export default class StockAdvice extends Component {
                 minLine={1}
                 url='https://www.nerdwallet.com/article/investing/how-to-invest-in-stocks'
                 />
-                <img src='https://www.nerdwallet.com/assets/blog/wp-content/uploads/2017/02/GettyImages-617563676-1920x1152.jpg' alt='desk'/>
+                <a href='https://www.nerdwallet.com/article/investing/how-to-invest-in-stocks' target='_blank' rel='noreferrer'><img src='https://www.nerdwallet.com/assets/blog/wp-content/uploads/2017/02/GettyImages-617563676-1920x1152.jpg' alt='desk'/></a>
                 <ReactTinyLink
                 cardSize="large"
                 showGraphic={false}
@@ -22,7 +22,7 @@ export default class StockAdvice extends Component {
                 minLine={1}
                 url='https://www.clevergirlfinance.com/blog/how-to-start-investing/'
                 />
-                <img src='https://3mizy13z0lac30t3qw3dz1bj-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/Investing-tips-for-women.jpg' alt='person reading newspaper'/>
+                <a href='https://www.clevergirlfinance.com/blog/how-to-start-investing/' target='_blank' rel='noreferrer'><img src='https://3mizy13z0lac30t3qw3dz1bj-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/Investing-tips-for-women.jpg' alt='person reading newspaper'/></a>
                 <ReactTinyLink
                 cardSize="large"
                 showGraphic={false}
@@ -30,14 +30,7 @@ export default class StockAdvice extends Component {
                 minLine={1}
                 url='https://www.nerdwallet.com/article/investing/investments-for-beginners'
                 />
-                <img src='https://www.nerdwallet.com/assets/blog/wp-content/uploads/2019/03/gettyimages-599689268-story-770x462.jpg' alt='person holding baby'/>
-                <ReactTinyLink
-                cardSize="large"
-                showGraphic={false}
-                maxLine={2}
-                minLine={1}
-                url='https://www.nerdwallet.com/article/investing/investments-for-beginners'
-                />
+                <a href='https://www.nerdwallet.com/article/investing/investments-for-beginners' target='_blank' rel='noreferrer'><img src='https://www.nerdwallet.com/assets/blog/wp-content/uploads/2019/03/gettyimages-599689268-story-770x462.jpg' alt='person holding baby'/></a>
             </div>
         )
     }
