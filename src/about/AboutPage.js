a1abbda09790207fab85af02256aac9999f90fcd
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-03-11 13:49:12 -0800

    create about us page

--- src/about/AboutPage.js
+++ src/about/AboutPage.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import ImageItem from './ImageItem.js';
+import BioItem from './BioItem.js';
+import MediaItem from './MediaItem.js';
+import teamBios from './bios.js';
+import './about.css';
 
 export default class AboutPage extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="bio-page">
+                {teamBios.map(person => 
+                    <div key={person.name} className="bio-items">
+                        <h3>{person.name}</h3>
+                        <ImageItem image={person.image} className="bio-image" />
+                        <BioItem bio={person.bio} />
+                        <MediaItem linkedin={person.linkedin} github={person.github} />
+                    </div>
+                )}
             </div>
         )
     }
--- src/about/MediaItem.js
+++ src/about/MediaItem.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import  linkedinImage from './assets/linkedin.png';
+import githubImage from './assets/github.png';
 
 export default class MediaItem extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="media-links">
+                <a href={this.props.linkedin}>
+                    <img src={githubImage} />
+                </a>
+                <a href={this.props.github}>
+                    <img src={linkedinImage} />
+                </a>
             </div>
         )
     }
